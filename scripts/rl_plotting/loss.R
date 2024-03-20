319b6450968b450c079d8ded449126aff0fd594a
Hochan Lee <hochan@utexas.edu>
2024-03-20 14:51:52 -0500

    Fix plot scripts

--- scripts/rl_plotting/loss.R
+++ scripts/rl_plotting/loss.R
@@ -7,11 +7,12 @@ library(RColorBrewer)
 library(ggh4x)
 library(ggbreak)
 
-loss_data <- read.csv("/Users/hochanlee/workplace/rl/20240320/outputs/test.loss", header=T, fileEncoding="UTF-8-BOM")
+args=commandArgs(trailingOnly=TRUE)
+
+loss_data <- read.csv(args[1], header=T, fileEncoding="UTF-8-BOM")
 print(loss_data)
 line_graph <- ggplot() +
-  geom_point(data=loss_data, aes(x=Iteration, y=Loss), color="blue") +
+  geom_point(data=loss_data, aes(x=Iterations, y=Loss), color="blue") +
   labs(x="Iterations", y = "Loss")
 line_graph
-#ggsave("loss.pdf", height=6, width=6 )
-
+ggsave("loss.pdf", height=6, width=6 )
