552611e41ec02d49d42b56a85101df554fc67896
Hochan Lee <hochan@utexas.edu>
2024-05-19 15:51:23 -0500

    Refine r script

--- scripts/rl_plotting/simtime.R
+++ scripts/rl_plotting/simtime.R
@@ -23,29 +23,35 @@ print (data)
 data <- data_summary(data, varname="ExecutionTime", groupnames=c("Mode"))
 
 data$Mode <- factor(data$Mode, levels=c(
-  "Independent", "BSP", "HEFTTheory", "Serial", "heft", "loadbalance", "eft_without_data", "eft_with_data", "random"))
+  "Serial", "BSP", "Independent", "HEFTTheory", "heft", "loadbalance", "eft_without_data", "eft_with_data", "random"))
+levels(data$Mode)[levels(data$Mode) == "eft_without_data"] <- "EFT-NoData"
+levels(data$Mode)[levels(data$Mode) == "eft_with_data"] <- "EFT"
+levels(data$Mode)[levels(data$Mode) == "random"] <- "Random"
+levels(data$Mode)[levels(data$Mode) == "loadbalance"] <- "LoadBalance"
+
+data$vlines <- factor(data$Mode, labels=c(
+  "Theory", "Theory", "Theory", "Offline", "Online", "Online", "Online", "Online", "Online")) 
 
 print(data$Mode)
 print(data)
-time_plot <- ggplot(data=data, aes(x=factor(Mode), fill=Mode, y=ExecutionTime
-)) +
-  scale_x_discrete(labels=c("Independent", "BSP", "HEFTTheory",  "Serial", "HEFT", "LoadBalance", "EFT-NoData", "EFT", "Random"))+
-  geom_bar(mapping=aes(x=factor(Mode), fill=Mode, y=ExecutionTime
-),
-           position=position_dodge(preserve='single'), stat="identity") +
-  labs(fill="Methods", y="Execution time (s)", x="")+
-  theme(axis.title = element_text(color="black", size=20),
-        axis.text.y = element_text(color="black", size=20),
-        axis.text.x = element_text(color="black", size=20, angle=90),
-        axis.title.y = element_text(size=20),
-        axis.title.x = element_text(size=20),
-        legend.position="none") +
-  geom_text(aes(label=sprintf("%1.2f", ExecutionTime
-)),
-            position=position_dodge(1), vjust=0) +
-  geom_errorbar(aes(ymin=ExecutionTime
--sd, ymax=ExecutionTime
-+sd), width=.2, position=position_dodge(.9))
+time_plot <- ggplot(data=data, aes(x=Mode, fill=Mode, y=ExecutionTime)) +
+             # scale_x_discrete(labels=c("Serial", "BSP", "Independent", "HEFTTheory",  "HEFT", "LoadBalance", "EFT-NoData", "EFT", "Random"))+
+             geom_bar(
+                      #mapping=aes(x=factor(Mode), fill=Mode, y=ExecutionTime),
+                      position=position_dodge(preserve='single'), stat="identity") +
+             labs(fill="Methods", y="Execution time (s)", x="")+
+             theme(axis.title = element_text(color="black", size=20),
+                   axis.text.y = element_text(color="black", size=20),
+                   axis.text.x = element_text(color="black", size=20, angle=90),
+                   axis.title.y = element_text(size=20),
+                   axis.title.x = element_text(size=20),
+                   legend.position="none") +
+             # facet_wrap(~vlines, scales="free_x") +
+             facet_grid(. ~vlines, scales="free_x", space="free") +
+             geom_text(aes(label=sprintf("%1.2f", ExecutionTime)),
+             position=position_dodge(1), vjust=0) +
+             geom_errorbar(aes(ymin=ExecutionTime-sd, ymax=ExecutionTime+sd),
+             width=.2, position=position_dodge(.9))
 
 time_plot
 ggsave(args[2], height=6, width=10)
