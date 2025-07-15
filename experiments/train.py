b1f78910254adeaa0691fafbdc18bab344d4e025
wlruys <will.ruys@gmail.com>
2025-07-15 18:14:57 -0500

    clean: add version logging

--- experiments/train.py
+++ experiments/train.py
@@ -1,38 +1,58 @@
-# src/train.py
 import hydra
 from omegaconf import DictConfig, OmegaConf
-from task4feedback.ml.models import *
-from task4feedback.ml.util import *
-from task4feedback.ml.env import *
-from task4feedback.ml.algorithms import *
-from task4feedback.interface import TaskTuple
 import wandb
 from hydra.utils import instantiate
 
 from helper.graph import make_graph_builder
 from helper.env import make_env
-from helper.model import *
-from functools import partial
+from helper.model import create_td_actor_critic_models
+from helper.algorithm import create_optimizer, create_lr_scheduler
 
 from task4feedback.ml.algorithms.ppo import run_ppo, run_ppo_lstm
-
+from task4feedback.interface.wrappers import *
+from task4feedback.ml.models import *
 # torch.multiprocessing.set_sharing_strategy("file_descriptor")
 # torch.multiprocessing.set_sharing_strategy("file_system")
 
-from rich import print as rprint
-from torchrl.envs.utils import check_env_specs
-from torchrl.envs import ParallelEnv
+from hydra.experimental.callbacks import Callback
+from hydra.core.utils import JobReturn
+from omegaconf import DictConfig, open_dict
+from pathlib import Path
+import git
+import os 
+from hydra.core.hydra_config import HydraConfig
+
+
+class GitInfo(Callback):
+    def on_job_start(self, config: DictConfig, **kwargs) -> None:
+        try:
+            repo = git.Repo(search_parent_directories=True)
+            outdir = Path(config.hydra.runtime.output_dir)
+            outdir.mkdir(parents=True, exist_ok=True)
+            (outdir / "git_sha.txt").write_text(repo.head.commit.hexsha)
+            (outdir / "git_dirty.txt").write_text(str(repo.is_dirty()))
+            diff = repo.git.diff(None)
+            (outdir / "git_diff.patch").write_text(diff)
+
+            print(
+                "Git SHA:",
+                repo.head.commit.hexsha,
+                " (dirty)" if repo.is_dirty() else " (clean)",
+                flush=True,
+            )
+
+        except Exception as e:
+            print(f"GitInfo callback failed: {e}")
 
 
 def configure_training(cfg: DictConfig):
+    start_logger()
     graph_builder = make_graph_builder(cfg)
     env, normalization = make_env(graph_builder=graph_builder, cfg=cfg)
 
     observer = env.get_observer()
     feature_config = FeatureDimConfig.from_observer(observer)
-    print(f"Feature config: {feature_config}")
-
-    a, lstm = create_td_actor_critic_models(cfg, feature_config)
+    model, lstm = create_td_actor_critic_models(cfg, feature_config)
 
     def env_fn():
         return make_env(
@@ -40,6 +60,9 @@ def configure_training(cfg: DictConfig):
         )
 
     alg_config = instantiate(cfg.algorithm)
+    
+    optimizer = create_optimizer(cfg)
+    lr_scheduler = create_lr_scheduler(cfg)
 
     if cfg.wandb.enabled:
         logging_config = instantiate(cfg.logging)
@@ -48,17 +71,21 @@ def configure_training(cfg: DictConfig):
 
     if lstm is not None:
         run_ppo_lstm(
-            actor_critic_module=a,
+            actor_critic_module=model,
             env_constructors=[env_fn],
             logging_config=logging_config,
             ppo_config=alg_config,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
         )
     else:
         run_ppo(
-            actor_critic_module=a,
+            actor_critic_module=model,
             env_constructors=[env_fn],
             logging_config=logging_config,
             ppo_config=alg_config,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
         )
     
 
@@ -72,6 +99,14 @@ def main(cfg: DictConfig):
             name=cfg.wandb.name,
             dir=cfg.wandb.dir,
         )
+        
+        hydra_output_dir = Path(HydraConfig.get().runtime.output_dir)
+
+        with open_dict(cfg):
+            for fname in ["git_sha.txt", "git_diff.patch", "git_dirty.txt"]:
+                git_file = hydra_output_dir / fname
+                if git_file.exists():
+                    wandb.save(str(git_file))
 
     configure_training(cfg)
 
