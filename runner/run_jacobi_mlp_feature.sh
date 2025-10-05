a3a1ca1f07f49dd50f91926c11e2cf0592a9ad91
wlruys <will.ruys@gmail.com>
2025-10-05 13:48:57 -0500

    test: move feature test to spr nodes

--- runner/run_jacobi_mlp_feature.sh
+++ runner/run_jacobi_mlp_feature.sh
@@ -6,4 +6,4 @@ python runner/expgen.py build \
 --out runner/jacobi_mlp_feature/outputs \
 --batch-size 24 --nonstrict
 
-python runner/expgen.py slurm --yaml runner/jacobi_mlp_feature/experiment.yaml --out runner/jacobi_mlp_feature/outputs --launcher '$PWD/runner/run_tmux_launcher.sh' --k-per-session 4 --job-name jacobi_mlp_feature --batch-size 24 --slurm-logs runner/jacobi_mlp_feature/logs --partition skx --time "08:00:00"
+python runner/expgen.py slurm --yaml runner/jacobi_mlp_feature/experiment.yaml --out runner/jacobi_mlp_feature/outputs --launcher '$PWD/runner/run_tmux_launcher.sh' --k-per-session 4 --job-name jacobi_mlp_feature --batch-size 24 --slurm-logs runner/jacobi_mlp_feature/logs --partition spr --time "08:00:00"
